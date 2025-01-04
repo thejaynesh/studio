3787135562ca7372ffdd46f0873eee0e225fafef
Jaynesh Bhandari <thejaynesh@gmail.com>
2025-01-03 20:00:43 -0500

    improving directory

--- lib/View/HomeScreen.dart
+++ lib/View/HomeScreen.dart
@@ -52,7 +52,7 @@ class _HomeScreenState extends State<HomeScreen> {
                     decoration: const BoxDecoration(
                       borderRadius: BorderRadius.all(Radius.circular(20)),
                       boxShadow: [
-                        const BoxShadow(
+                         BoxShadow(
                           color: Colors.black26,
                           blurRadius: 6,
                           offset: Offset(2, 2),
